600bd0611bd565e378f5aec2ccd6e0b33776c8d4
Walter Okas <walter@dmarkmobile.com>
2022-08-22 23:40:50 +0300

    First Run App successfully

--- app.py
+++ app.py
@@ -4,7 +4,7 @@
 
 import json
 import dateutil.parser
-#import babel
+import babel
 from flask import Flask, render_template, request, Response, flash, redirect, url_for
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
